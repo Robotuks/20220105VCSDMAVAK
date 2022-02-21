0a1d524a245002bf789fb5d434c61a8345181106
Robotuks <grigdarius@gmail.com>
2022-02-21 19:04:50 +0200

    lesson 2

---
+++ test.py
@@ -0,0 +1,11 @@
+number = 47
+print (number + 5)
+print (number - 5)
+print (number * 5)
+print (number / 5)
+print (number ** 5) # kelimas laipsniu
+print (2 ** 3)
+print (number // 5) # sveikaja dali
+print (number % 5) # likutine dalis
+print (3 % 2) # likutine dalis
+print (6 % 2) # likutine dalis
