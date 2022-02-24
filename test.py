7ffa4df1f147421540f79adf51ec434c1f4de728
Robotuks <grigdarius@gmail.com>
2022-02-24 21:39:00 +0200

    lesson 5

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
-number = 47
-print (number + 5)
-print (number - 5)
-print (number * 5)
-print (number / 5)
-print (number ** 5) # kelimas laipsniu
-print (2 ** 3)
-print (number // 5) # sveikaja dali
-print (number % 5) # likutine dalis
-print (3 % 2) # likutine dalis
-print (6 % 2) # likutine dalis
+# number = 47
+# print (number + 5)
+# print (number - 5)
+# print (number * 5)
+# print (number / 5)
+# print (number ** 5) # kelimas laipsniu
+# print (2 ** 3)
+# print (number // 5) # sveikaja dali
+# print (number % 5) # likutine dalis
+# print (3 % 2) # likutine dalis
+# print (6 % 2) # likutine dalis
 
-while True:
-    print ('to infinity and beyond')
+# while True:
+    # print ('to infinity and beyond')
+
+from math import * # NEDARYTI TAIP
+from math import sqrt, ceil
+
+print(sqrt(9) )
+
+
+def hello():
+    print ('Hello world')
+
